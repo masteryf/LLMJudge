c071d3c2bfdeca5f7d5e7ff73702029f2be2dbb6
YF <647831033@qq.com>
2025-03-29 09:27:43 +0800

    add hle

---
+++ tools/parquet2csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+df = pd.read_parquet("C:\\Users\\YF\\Downloads\\0000.parquet")
+
+# 过滤空image字段（模型不是多模态的，在纯文本子集上评估）
+filtered_df = df[df['image'] == '']
+
+
+filtered_df = filtered_df.assign(score=1)
+filtered_df[['question', 'answer', 'score']].to_csv('Humanity'sLastExam.csv', index=False)
+
+print("CSV文件生成完成，包含 {} 条数据".format(len(filtered_df)))
